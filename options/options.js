2cd6898911149d99e51fc2a149972fbb70a1c80e
Crowdbotics <team@crowdbotics.com>
2023-09-07 09:28:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-dream-43261",
+  url: "https://wild-dream-43261.botics.co",
+  api: "https://wild-dream-43261.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
